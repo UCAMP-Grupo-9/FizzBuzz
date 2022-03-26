1ab7e0bb9c04b004f3b55626faff516122c59958
Sebastian <jsebastpz@gmail.com>
2022-03-26 10:17:06 -0500

    mejoras

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,7 +1,7 @@
 
 function fizzbuzz(){
     let newArray = [];
-    for (var i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if ((i % 3) && (i % 5) == 0) newArray.push("FizzBuzz");
         else if (i % 3 == 0) newArray.push("Fizz");
         else if (i % 5 == 0) i = newArray.push("Buzz");
